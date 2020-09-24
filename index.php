d8d5f52ac65455ab9bb757a71ffb10e4950ba380
NoelGonza <71825809+NoelGonza@users.noreply.github.com>
2020-09-24 13:44:48 +0200

    Update index.php

--- index.php
+++ index.php
@@ -12,6 +12,7 @@ and open the template in the editor.
     <body>
         <?php
             echo "<h2>Vas a hacer login con el usuario</h2>";
+            echo "<p>No has hecho login</p>";
         ?>
     </body>
 </html>
